5223f1e1f84e991fb92e465413dd564d41fd8a83
Asterios Ntinos <ntinosasterios@gmail.com>
2021-01-05 21:05:29 +0200

    [TEST] update interval new option

--- src/components/GlobalConfigsCmp.vue
+++ src/components/GlobalConfigsCmp.vue
@@ -2,12 +2,26 @@
   <b-container>
     <h3>Global Configuration</h3>
     <hr />
-    <p class="lead">Here you can customize the overall behavior of the crawler.</p>
+    <p class="lead">
+      Here you can customize the overall behavior of the crawler.
+    </p>
     <div>
-      <b-card title="Set Global Parameters" style="max-width: 60rem;" class="mb-2">
+      <b-card
+        title="Set Global Parameters"
+        style="max-width: 60rem"
+        class="mb-2"
+      >
         <validation-observer ref="observer" v-slot="{ passes }">
-          <b-form @submit.stop.prevent="passes(onSubmit)" @reset="onReset" v-if="show">
-            <validation-provider name="Update Interval" rules="required" v-slot="validationContext">
+          <b-form
+            @submit.stop.prevent="passes(onSubmit)"
+            @reset="onReset"
+            v-if="show"
+          >
+            <validation-provider
+              name="Update Interval"
+              rules="required"
+              v-slot="validationContext"
+            >
               <b-form-group
                 id="input-group-1"
                 label="Update Database Interval"
@@ -21,9 +35,9 @@
                   :state="getValidationState(validationContext)"
                   aria-describedby="input-1-live-feedback"
                 ></b-form-select>
-                <b-form-invalid-feedback
-                  id="input-1-live-feedback"
-                >{{ validationContext.errors[0] }}</b-form-invalid-feedback>
+                <b-form-invalid-feedback id="input-1-live-feedback">{{
+                  validationContext.errors[0]
+                }}</b-form-invalid-feedback>
               </b-form-group>
             </validation-provider>
 
@@ -45,26 +59,41 @@
                   :state="getValidationState(validationContext)"
                   aria-describedby="input-2-live-feedback"
                 ></b-form-select>
-                <b-form-invalid-feedback
-                  id="input-2-live-feedback"
-                >{{ validationContext.errors[0] }}</b-form-invalid-feedback>
+                <b-form-invalid-feedback id="input-2-live-feedback">{{
+                  validationContext.errors[0]
+                }}</b-form-invalid-feedback>
               </b-form-group>
             </validation-provider>
-              <div class="d-flex flex-row">
-              <p class="mr-2" v-if="showLoadingMessage">Sending data to server...</p>
+            <div class="d-flex flex-row">
+              <p class="mr-2" v-if="showLoadingMessage">
+                Sending data to server...
+              </p>
               <p
                 v-if="showMessage"
                 v-bind="resultMessage"
-                v-bind:class="{'text-success': isSuccess, 
-                                'text-danger': !isSuccess
-                                }"
+                v-bind:class="{
+                  'text-success': isSuccess,
+                  'text-danger': !isSuccess,
+                }"
               >
                 <b>{{ resultMessage }}</b>
               </p>
-              <b-spinner class="spinner-border-sm mt-1" v-if="showSpinner" variant="primary"></b-spinner>
-              </div>
-            <b-button class="mr-2" type="submit" variant="success" v-bind:disabled="isDisabled">Submit</b-button>
-            <b-button type="reset" v-bind:disabled="isDisabled">Defaults</b-button>
+              <b-spinner
+                class="spinner-border-sm mt-1"
+                v-if="showSpinner"
+                variant="primary"
+              ></b-spinner>
+            </div>
+            <b-button
+              class="mr-2"
+              type="submit"
+              variant="success"
+              v-bind:disabled="isDisabled"
+              >Submit</b-button
+            >
+            <b-button type="reset" v-bind:disabled="isDisabled"
+              >Defaults</b-button
+            >
           </b-form>
         </validation-observer>
       </b-card>
@@ -78,7 +107,7 @@ export default {
     return {
       globalConfig: {
         updateFreq: 12,
-        crawlFreq: 24
+        crawlFreq: 24,
       },
       show: true,
       showSpinner: false,
@@ -88,20 +117,21 @@ export default {
       isDisabled: false,
       isSuccess: false,
       updateFreqOptions: [
+        { value: 1, text: "Every 1 hour" },
         { value: 3, text: "Every 3 hours" },
         { value: 6, text: "Every 6 hours" },
         { value: 9, text: "Every 9 hours" },
-        { value: 12, text: "Every 12 hours" }
+        { value: 12, text: "Every 12 hours" },
       ],
       crawlFreqOptions: [
         { value: 18, text: "Every 18 hours" },
         { value: 24, text: "Every 24 hours" },
-        { value: 36, text: "Every 36 hours" }
-      ]
+        { value: 36, text: "Every 36 hours" },
+      ],
     };
   },
-  created(){
-    this.BASE_URL = "https://crawler-admin-config-be.herokuapp.com"
+  created() {
+    this.BASE_URL = "https://crawler-admin-config-be.herokuapp.com";
   },
   methods: {
     getValidationState({ dirty, validated, valid = null }) {
@@ -109,29 +139,32 @@ export default {
     },
     onSubmit() {
       console.log(JSON.stringify(this.globalConfig));
-      //alert("Changes submited!");
-      let uri = this.BASE_URL + "/seeds/configure"
+      let uri = this.BASE_URL + "/seeds/configure";
       this.showLoadingMessage = true;
       this.showSpinner = true;
       this.showMessage = false;
       this.isDisabled = true;
+      this.globalConfig["username"] = sessionStorage.getItem("username");
+      this.globalConfig["password"] = sessionStorage.getItem("password");
       this.axios
         .post(uri, this.globalConfig, {
           headers: {
-            Authorization: `Bearer ${sessionStorage.getItem("token")}`
-          }
-        }).then(res => {
+            Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+          },
+        })
+        .then((res) => {
           this.resultMessage = "Settings sent!";
           this.showLoadingMessage = false;
           this.showMessage = true;
           this.showSpinner = false;
           this.isSuccess = true;
           this.isDisabled = false;
-          console.log(res)
+          console.log(res);
           setTimeout(() => {
             location.reload();
           }, 800);
-        }).catch(err => {
+        })
+        .catch((err) => {
           this.showLoadingMessage = false;
           this.showSpinner = false;
           this.isSuccess = false;
@@ -139,8 +172,8 @@ export default {
             "An Error occured: " + err.message.replace("Error:", "");
           this.showMessage = true;
           this.isDisabled = false;
-          console.log(err)
-        })
+          console.log(err);
+        });
     },
     onReset(evt) {
       evt.preventDefault();
@@ -152,8 +185,8 @@ export default {
       this.$nextTick(() => {
         this.show = true;
       });
-    }
-  }
+    },
+  },
 };
 </script>
 
